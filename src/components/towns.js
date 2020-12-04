cf5d4a8c377ddce70e8fcc7e6f5d657651264974
ashwinvem <51525902+ashwinvem@users.noreply.github.com>
2020-12-04 12:42:39 +0530

    Update towns.js

--- src/components/towns.js
+++ src/components/towns.js
@@ -1,13 +1,8 @@
+
 import React from "react";
 
-export default function Towns({ inputState }) {
-  return (
-    <>
-      {inputState.towns.map((town, index) => (
-        <div key={town.name} id={`town${index + 1}`}>
-          {town.name}
-        </div>
-      ))}
-    </>
-  );
+import "../styles/App.css";
+
+export default function Towns(props) {
+  return <div className="townSpace">{props.town.name}</div>;
 }
